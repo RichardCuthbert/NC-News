d42926d94b3cfd9d373a535cc1d348971d863e28
RichardCuthbert <RichardCuthbertnc@protonmail.com>
2022-01-18 12:20:25 +0000

    Implement GET /api/topics endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,3 +1,5 @@
+const request = require("supertest");
+const app = require("../app");
 const db = require("../db/connection");
 const testData = require("../db/data/test-data/index.js");
 const seed = require("../db/seeds/seed");
@@ -5,3 +7,24 @@ const seed = require("../db/seeds/seed");
 beforeEach(() => seed(testData));
 
 afterAll(() => db.end());
+
+describe("/api/topics", () => {
+  describe("/GET", () => {
+    it("status:200 and returns an array of topics", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then((res) => {
+          expect(res.body.topics).toHaveLength(3);
+          res.body.topics.forEach((topic) => {
+            expect(topic).toEqual(
+              expect.objectContaining({
+                topic_name: expect.any(String),
+                topic_description: expect.any(String),
+              })
+            );
+          });
+        });
+    });
+  });
+});
---
+++ models/topics.models.js
@@ -0,0 +1,8 @@
+const res = require("express/lib/response");
+const db = require("../db/connection");
+
+exports.fetchTopics = () => {
+  return db.query("SELECT * FROM topics;").then((result) => {
+    return result.rows;
+  });
+};
