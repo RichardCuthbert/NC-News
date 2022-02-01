1c3d130f0822958ff07b716c60a98ea4c70ba3a2
RichardCuthbert <RichardCuthbertnc@protonmail.com>
2022-02-01 08:57:07 +0000

    Config

--- routers/api.router.js
+++ routers/api.router.js
@@ -1,10 +1,10 @@
+const apiRouter = express.Router();
 const topicsRouter = require("./topics.router");
 const articlesRouter = require("./articles.router");
 const commentsRouter = require("./comments.router");
 const { getApi } = require("../controllers/api.controller");
 
 const express = require("express");
-const apiRouter = express.Router();
 
 apiRouter.use("/topics", topicsRouter);
 
